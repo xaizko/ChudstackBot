25cd5295f2cc845fdf350c68cb08279a132ea6d1
xaizko <tonytran177013@gmail.com>
2026-04-23 13:35:18 -0400

    weird formatting

--- src/features/metroReact.ts
+++ src/features/metroReact.ts
@@ -1,9 +1,9 @@
 import type { Message } from "discord.js";
 
 export async function metroReact(message: Message) {
-    if (message.author.bot) return;
-    
-    if (message.content.toLowerCase().includes("metro")) {
-        await message.react("1495344198151311502");
-    }
+	if (message.author.bot) return;
+
+	if (message.content.toLowerCase().includes("metro")) {
+		await message.react("1495344198151311502");
+	}
 }
