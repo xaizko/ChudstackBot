418a09de48c23682d0c05e6fc3d6118f6dcc6ab0
xaizko <tonytran177013@gmail.com>
2026-04-23 13:34:42 -0400

    updated getSteam32Id

--- src/utility/getSteamId.ts
+++ src/utility/getSteamId.ts
@@ -1,8 +1,11 @@
-export async function getSteam32Id(id: number) {
+export async function getSteam32Id(id: bigint) {
 	const response = await fetch(`https://steamid.xyz/${id}`);
 	const html = await response.text();
 
 	const steamId = html.match(/Steam ID ([0-9]+)/);
+	if (!steamId?.[1]) {
+		throw new Error("Could not parse Steam32 ID");
+	}
 
-	return steamId;
+	return steamId[1];
 };
