d795e5e72a08877068500aab000421d23f9c6d1f
xaizko <tonytran177013@gmail.com>
2026-04-23 13:13:36 -0400

    utility to get steam32id

---
+++ src/utility/getSteamId.ts
@@ -0,0 +1,8 @@
+export async function getSteam32Id(id: number) {
+	const response = await fetch(`https://steamid.xyz/{id}`);
+	const html = await response.text();
+
+	const steamId = html.match(/Steam ID ([0-9]+)/);
+
+	return steamId;
+};
