204798dc4c90dee0afb9903610111ca838f2ca2f
xaizko <tonytran177013@gmail.com>
2026-04-23 13:22:51 -0400

    fix not using id

--- src/utility/getSteamId.ts
+++ src/utility/getSteamId.ts
@@ -1,5 +1,5 @@
 export async function getSteam32Id(id: number) {
-	const response = await fetch(`https://steamid.xyz/{id}`);
+	const response = await fetch(`https://steamid.xyz/${id}`);
 	const html = await response.text();
 
 	const steamId = html.match(/Steam ID ([0-9]+)/);
