f8fe9a1328e5b39a720b7933261d16e1c4bb9f4c
xaizko <tonytran177013@gmail.com>
2026-04-25 23:38:55 -0400

    allowed export of database

--- src/loaders/loadDb.ts
+++ src/loaders/loadDb.ts
@@ -1,5 +1,5 @@
 import Database from "better-sqlite3";
-const db = new Database("data/chudstack.db");
+export const db = new Database("data/chudstack.db");
 
 db.pragma("journal_mode = WAL");
 db.pragma("foreign_keys = ON");
