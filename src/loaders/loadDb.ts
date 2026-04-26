ad0157bed8fc9edb2d82c481db54875833a02671
xaizko <tonytran177013@gmail.com>
2026-04-25 23:59:29 -0400

    fixed directory

--- src/loaders/loadDb.ts
+++ src/loaders/loadDb.ts
@@ -1,5 +1,5 @@
 import Database from "better-sqlite3";
-export const db = new Database("data/chudstack.db");
+export const db = new Database("chudstack.db");
 
 db.pragma("journal_mode = WAL");
 db.pragma("foreign_keys = ON");
