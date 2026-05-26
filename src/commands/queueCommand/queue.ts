6fadf82f55ef4f595e380b7b65997d0745cc22e4
xaizko <tonytran177013@gmail.com>
2026-05-26 00:32:35 -0400

    added voice channel as option to queue

--- src/commands/queueCommand/queue.ts
+++ src/commands/queueCommand/queue.ts
@@ -1,15 +1,20 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction } from "discord.js";
+import { SlashCommandBuilder, ChatInputCommandInteraction, ChannelType } from "discord.js";
 
 const queueCommand = {
 	data: new SlashCommandBuilder()
 		.setName("queue")
-		.setDescription("Start a session"),
+		.setDescription("Start a session")
+		.addChannelOption(option => option
+				   .setName("channel")
+				   .setDescription("Channel to join")
+				   .setRequired(true)
+				   .addChannelTypes(ChannelType.GuildVoice)),
 		async execute(interaction: ChatInputCommandInteraction) {
 			const user = interaction.user;
 			const timeObject = new Date();
 			const timeStarted = Math.floor(timeObject.getTime() / 1000); // Milli to Seconds
 
-			await interaction.reply(`${user} started a chudstack at <t:${timeStarted}>`);
+			await interaction.reply(`${user} started a chudstack at <t:${timeStarted}> in ${interaction.options.getChannel("channel")}`);
 
 			// TODO:
 			// Track start and end time
